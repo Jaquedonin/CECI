745f0b169b7d2732af15216753e813d1d71cd4c3
Jaqueline Donin <40238428+Jaquedonin@users.noreply.github.com>
2020-02-06 11:57:38 -0300

    Estrutura da máquina de estado com S0 e S1

---
+++ State_machine.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+int test_Reset(int reset, int clock_atual)
+{
+	
+		
+	//teste de reset
+	if(reset == 1)
+		return
+		
+	
+		 
+		
+	return decode(int clock_atual);
+
+	
+}
+
+int Decode(int clock_atual)
+{
+	
+	if(clock_atual)	{
+	// decodifica a instrução lida em fetch
+		printf("Estado atual em S1")
+		clock_atual = 0;
+		//chama próximo estado
+	}
+	else {
+		printf("COntinua em estado anterior S0")
+		clock_atual= 1;
+	}
+	
+	
+
+	
+}
+
+
+
+int main(int argc, char const *argv[])
+{	
+	clock_proximo = 1;
+	clock_atual = 0;
+	
+
+	if(clock_atual==0)
+		
+		//ler arquivo linha a linha
+			// lê primeiro bit do arquivo
+			//chamar test_Reset
+		clock_atual= clock_proximo;	
+	else
+		clock_proximo = 0;
+		
+		//print descida do clock, 
+	
+}
